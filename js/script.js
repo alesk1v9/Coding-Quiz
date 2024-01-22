796cae9c43b65c0686dcfee5c15a937a87d2c085
Alexsander Rafael <alexsanderrafael2022@gmail.com>
2024-01-21 20:26:28 -0500

    added questions

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// QUESTIONS
+
+var questions = [
+    {
+      question: "Commonly used data types DO Not include:",
+      options: ["1. strings", "2. booleans", "3. alerts", "4. number"],
+      correctAnswer: 2
+    },
+    {
+      question: "The condition in an if/else statement is enclosed with ",
+      options: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+      correctAnswer: 2
+    },
+    {
+      question: "Arrays in JavaScript can be used to store ",
+      options: ["1. number and strings", "2. other array", "3. booleans", "4. all of the above"],
+      correctAnswer: 3
+    },
+    {
+      question: "String values must be enclosed within _____ when being assigned to variables.",
+      options: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+      correctAnswer: 2
+    },
+    {
+      question: "A very useful tool used during development and debbunging for printing content to the debbuger is:",
+      options: ["1. JavaScript", "2. Terminal/Bash", "3. for loops", "4. console.log"],
+      correctAnswer: 3
+    }
+  ];
